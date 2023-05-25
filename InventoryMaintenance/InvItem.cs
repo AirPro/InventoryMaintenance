b8e18749d436b74d388cc3de51c2eb16776deaaa
Bob Freid <17238974+AirPro@users.noreply.github.com>
2023-05-25 14:46:33 -0400

    Add project files.

---
+++ InventoryMaintenance/InvItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    internal class InvItem
+    {
+        public InvItem() { }
+
+        public InvItem(int itemNo, string description, decimal price) 
+        {
+            this.ItemNo = itemNo;
+            this.Description = description;
+            this.Price = price;
+        }
+
+        public int ItemNo { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public string GetDisplayText(string sep)
+        {
+            return $"{ItemNo}{sep}{Description}{sep}{Price.ToString("C")}";
+        }
+    }
+}
